7f33940f90e35e33e49c997958200808f6cf2e21
swekshya2 <csit2081028_swekshya@achsnepal.edu.np>
2025-08-03 21:27:53 +0545

    Add files via upload

---
+++ L5Q2.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+	string s;
+	cout<<"Enter a string:";
+	cin>> s;
+	if (next_permutation(s.begin(), s.end() )){
+		cout<< "Next permutation (already largest)." << endl;
+
+	}
+	return 0;
+}
